4fa67b51e456bbad56b31b5368f937c791d85863
kaizali <80753301+kaizali@users.noreply.github.com>
2021-08-11 15:51:43 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,44 @@
+class Food {
+    
+    constructor(){
+        this.foodStock = 0
+        this.image = loadImage ("images/Milk.png");
+        this.lastFed;
+    }
+    updateFoodStock(foodStock){
+        this.foodStock = foodStock
+    }
+
+    getfedTime(lastFed){
+        this.lastFed = lastFed
+    }
+
+    deductfood(){
+        if(foodStock > 0){
+            this.foodStock = foodStock-1;
+        }
+    }
+
+    getFoodStock (){
+        return this.foodStock;
+    }
+
+    display(){
+       var  x = 80,  y = 100
+    imageMode(CENTER);
+    image(this.image, 720, 220, 70, 70);
+
+    if(this.foodStock !=0){
+        for (var i=0; i<this.foodStock; i++){
+            if(i%10==0){
+                x = 80;
+                y=y+50;
+            }
+            image(this.image, x, y, 50, 50);
+            x=x+30;
+        }
+    }
+
+
+    }
+}
